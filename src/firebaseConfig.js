988748d890c387d20970dc41045d7a4119cfb2c6
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-13 12:39:15 -0600

    Setup Firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import Firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD6t6QhBUQCX8DCX8aVM7ZR7N9olKfoPF8",
+  authDomain: "fir-authentication-96400.firebaseapp.com",
+  databaseURL: "https://fir-authentication-96400.firebaseio.com",
+  projectId: "fir-authentication-96400",
+  storageBucket: "",
+  messagingSenderId: "37971250436"
+};
+
+const firebaseApp = Firebase.initializeApp(config);
+
+const db = firebaseApp.database()
+export const dbMenuRef = db.ref('menu')
