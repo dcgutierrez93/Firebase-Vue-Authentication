212dd0e9a4ef766c5e98ea434fa956219e8a35aa
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-13 12:42:37 -0600

    Setting up New Upload File

---
+++ src/components/New.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+
+</template>
+
+<script>
+// Import Firbase Config
+import { dbMenuRef } from '../firebaseConfig'
+
+export default {
+    data() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="css">
+</style>
