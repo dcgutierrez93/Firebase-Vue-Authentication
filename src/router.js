fc2fc4b31a4f4b83f88b0cab19275a430ba8ad84
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-14 10:50:42 -0600

    Added Routes for new components

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Welcome from './components/welcome/Welcome.vue'
+import Secure from './components/secure/Secure.vue'
 import Login from './components/auth/Login.vue'
 import Register from './components/auth/Register.vue'
 
@@ -10,6 +12,16 @@ Vue.use(Router)
 let router = new Router({
     mode: 'history',
     routes: [
+        {
+            path: '/',
+            name: 'welcome',
+            component: Welcome
+        },
+        {
+            path: '/secure',
+            name: 'secure',
+            component: Secure
+        },
         {
             path: '/login',
             name: 'login',
