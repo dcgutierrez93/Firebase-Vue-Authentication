8919138db3b8b4fc019f6749fc4836e1601826d6
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-14 11:10:58 -0600

    Added font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
+<style>
+
+</style>
+
 <template>
-  <div id="app">
-      <faheader></faheader>
-      <router-view/>
-      <fafooter></fafooter>
-  </div>
+
+<div id="app">
+    <faheader></faheader>
+    <router-view/>
+    <fafooter></fafooter>
+</div>
+
 </template>
 
 <script>
+
 import Header from './components/header/Header.vue'
 import Footer from './components/footer/Footer.vue'
 
 export default {
-  name: 'app',
-  components: {
-      faheader: Header,
-      fafooter: Footer
-  }
+    name: 'app',
+    components: {
+        faheader: Header,
+        fafooter: Footer
+    }
 }
+
 </script>
-
-<style>
-
-</style>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,13 +1,38 @@
-<template lang="html">
-    <header>
+<style lang="css">
 
-    </header>
+</style>
+
+<template lang="html">
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+        <a class="navbar-brand" href="#">Navbar w/ text</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+            Navbar text with an inline element
+            </span>
+        </div>
+    </nav>
+</header>
+
 </template>
 
 <script>
-export default {
-}
-</script>
 
-<style lang="css">
-</style>
+export default {}
+
+</script>
