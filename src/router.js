940113056e1fe0f47e85678c65d6008b64894cc2
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-14 10:47:40 -0600

    Setup Router for Login and Register

--- src/router.js
+++ src/router.js
@@ -1,1 +1,26 @@
-// TODO: Install Router
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from './components/auth/Login.vue'
+import Register from './components/auth/Register.vue'
+
+
+Vue.use(Router)
+
+let router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        }
+    ]
+})
+
+export default router
