5e24e95dc9a4b519681bcb97a0a9633812de30ae
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-14 10:01:12 -0600

    Added Login Template

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+    <div class="container">
+        <form class="">
+            <div class="form-group">
+                <label>Email Address</label>
+                <input
+                    type="email"
+                    id="email"
+                    class="form-control"
+                    placeholder="Enter email...">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    class="form-control"
+                    placeholder="Enter password...">
+            </div>
+            <button class="btn btn-primary" type="button">Sign in</button>
+            <button class="btn btn-danger" type="button">Sign out</button>
+        </form>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
